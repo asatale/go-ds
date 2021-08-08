0b02ea89cea71a6947a6d2ee06cd9a4bada54c3e
Aniruddha Atale <Aniruddha.Atale@oasisworks.com>
2021-08-08 14:14:50 -0400

    Implementation for merge sort

---
+++ msort/msort.go
@@ -0,0 +1,85 @@
+package msort
+
+import (
+	_ "fmt"
+)
+
+//Compare function returns
+// -1 if first quantity is less than second
+//  0 if first quantity is same as second
+//  1 if first quantity is greater than second
+type Compare func(interface{}, interface{}) int
+
+//Sort does sorting on the data using merge-sort algorithm
+func Sort(data []interface{}, cmp Compare) []interface{} {
+	if len(data) == 0 {
+		return []interface{}{}
+	}
+	return msortIterative(data, cmp)
+}
+
+// msortRecursive is recursive implementation for merge-sort algorithm
+func msortRecursive(data []interface{}, cmp Compare) []interface{} {
+	var fn func([]interface{}, int, int) []interface{}
+
+	fn = func(data []interface{}, lo, hi int) []interface{} {
+		if lo < hi {
+			mid := int(lo + ((hi - lo) / 2))
+			s1 := fn(data, lo, mid)
+			s2 := fn(data, mid+1, hi)
+			result := make([]interface{}, 0, (hi - lo))
+			return merge(s1, s2, cmp, result)
+		}
+		return []interface{}{data[lo]}
+	}
+	return fn(data, 0, len(data)-1)
+}
+
+func msortIterative(data []interface{}, cmp Compare) []interface{} {
+	min := func(a, b int) int {
+		if a < b {
+			return a
+		}
+		return b
+	}
+
+	result := make([]interface{}, 0, len(data))
+	for interval := 1; interval < len(data)/2; interval = interval * 2 {
+		for i := 0; i < len(data); i = i + 2*interval {
+			var lo, mid, hi int
+			lo = i
+			mid = i + interval
+			hi = min(i+2*interval, len(data))
+			result = result[:0]
+			result = merge(data[lo:mid], data[mid:hi], cmp, result)
+			copy(data[lo:hi], result)
+		}
+	}
+	return data
+}
+
+// merge performs merge for two sorted slices to produce sorted slice
+func merge(s1, s2 []interface{}, cmp Compare, result []interface{}) []interface{} {
+	i, j := 0, 0
+	for i < len(s1) && j < len(s2) {
+		if cmp(s1[i], s2[j]) == 1 {
+			result = append(result, s2[j])
+			j = j + 1
+		} else {
+			result = append(result, s1[i])
+			i = i + 1
+		}
+	}
+
+	for i < len(s1) {
+		result = append(result, s1[i])
+		i = i + 1
+	}
+
+	for j < len(s2) {
+		result = append(result, s2[j])
+		j = j + 1
+	}
+
+	return result
+}
---
+++ msort/msort_test.go
@@ -0,0 +1,134 @@
+package msort
+
+import (
+	"math/rand"
+	"reflect"
+	"testing"
+	"time"
+)
+
+type testCase struct {
+	input  []interface{}
+	output []interface{}
+}
+
+func runTestcases(tests []testCase, cmp Compare, t *testing.T) {
+	for _, test := range tests {
+		result := Sort(test.input, cmp)
+		if !reflect.DeepEqual(result, test.output) {
+			t.Errorf("Failed. Expected: %v, Got: %v", test.output, test.input)
+		}
+	}
+}
+
+func dataset(size int) (data []interface{}) {
+	data = make([]interface{}, size)
+	for i := 0; i < size; i++ {
+		data[i] = i
+	}
+	return data
+}
+
+func randomize(data []interface{}) []interface{} {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(data), func(i, j int) { data[i], data[j] = data[j], data[i] })
+	return data
+}
+
+func reverse(data []interface{}) []interface{} {
+	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
+		data[i], data[j] = data[j], data[i]
+	}
+	return data
+}
+
+func TestIntegerSort(t *testing.T) {
+
+	testCases := []testCase{
+		{
+			input:  []interface{}{4, 3, 2, 1},
+			output: []interface{}{1, 2, 3, 4},
+		},
+		{
+			input:  []interface{}{1, 2, 3, 4},
+			output: []interface{}{1, 2, 3, 4},
+		},
+		{
+			input:  []interface{}{2, 1},
+			output: []interface{}{1, 2},
+		},
+		{
+			input:  []interface{}{3, 2, 4, 1, 5, 5, 6, 1},
+			output: []interface{}{1, 1, 2, 3, 4, 5, 5, 6},
+		},
+		{
+			input:  []interface{}{1, 1, 1, 1, 1, 1, 1, 1},
+			output: []interface{}{1, 1, 1, 1, 1, 1, 1, 1},
+		},
+		{
+			input:  []interface{}{1},
+			output: []interface{}{1},
+		},
+		{
+			input:  []interface{}{},
+			output: []interface{}{},
+		},
+		{ // Large data set -- randomized
+			input:  randomize(dataset(1000 * 1000)), // 1M data
+			output: dataset(1000 * 1000),
+		},
+		{ // Large data set -- worstcase
+			input:  reverse(dataset(100 * 100)), // 100K data
+			output: dataset(100 * 100),
+		},
+	}
+	runTestcases(testCases,
+		func(a, b interface{}) int {
+			v1 := a.(int)
+			v2 := b.(int)
+			if v1 == v2 {
+				return 0
+			} else if v1 < v2 {
+				return -1
+			}
+			return 1
+		},
+		t)
+}
+
+func TestStringSort(t *testing.T) {
+	testCases := []testCase{
+		{
+			input:  []interface{}{"b", "a"},
+			output: []interface{}{"a", "b"},
+		},
+		{
+			input:  []interface{}{"d", "c", "b", "a"},
+			output: []interface{}{"a", "b", "c", "d"},
+		},
+		{
+			input:  []interface{}{"A", "a", "AA", "aa"},
+			output: []interface{}{"A", "AA", "a", "aa"},
+		},
+		{
+			input:  []interface{}{"da", "ac", "zb", "a"},
+			output: []interface{}{"a", "ac", "da", "zb"},
+		},
+		{
+			input:  []interface{}{"a", "a", "a", "a"},
+			output: []interface{}{"a", "a", "a", "a"},
+		},
+	}
+	runTestcases(testCases,
+		func(a, b interface{}) int {
+			v1 := a.(string)
+			v2 := b.(string)
+			if v1 == v2 {
+				return 0
+			} else if v1 < v2 {
+				return -1
+			}
+			return 1
+		},
+		t)
+}
